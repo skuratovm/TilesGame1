d5a029a7e6b012c391f9216de64bc01f925b8903
Mikhail <maydibee@gmail.com>
2021-09-16 00:51:46 +0300

    upgrade arc4randomExtension

--- StandLect1/ViewController.swift
+++ StandLect1/ViewController.swift
@@ -10,7 +10,13 @@ import AudioToolbox
 
 extension Int{
     var arc4randomExtension: Int{
+        if self > 0{
         return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0{
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
     }
 }
 
